9c43d04eda790012520e76df1ae870de4528bd11
Melendez <remyakamelendez@gmail.com>
2022-06-10 10:52:14 -0500

    commit 1

---
+++ src/app/components/instructor2/instructor2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Instructor2Component } from './instructor2.component';
+
+describe('Instructor2Component', () => {
+  let component: Instructor2Component;
+  let fixture: ComponentFixture<Instructor2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Instructor2Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Instructor2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
